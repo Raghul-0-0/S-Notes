4432103653e68264134c2375452b0e7c8ebc2eb7
Raghul <raghulvishva1016@gmail.com>
2025-03-21 21:33:29 +0530

    added home route with bg color and dummy sidebar

---
+++ Frontend/src/components/Home.jsx
@@ -0,0 +1,22 @@
+import "../css/Home.css"
+const Home = () =>{
+
+    return (
+        <div className="HomePage">
+            <h1 className="">this is the home page</h1>
+            <div className="sidebar">
+                <ul>
+                    <li>Heading 1</li>
+                    <li>Heading 2</li>
+                    <li>Heading 3</li>
+                    <li>Heading 4</li>
+                </ul>
+            </div>
+
+        </div>
+
+        
+    )
+} 
+
+export default Home;
