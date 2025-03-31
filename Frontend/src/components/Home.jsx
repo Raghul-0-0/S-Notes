f5dba98410f7f55baf847b84cc8c519d4499aed3
Raghul <raghulvishva1016@gmail.com>
2025-03-31 19:54:56 +0530

    added resize-able sidebar

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -3,15 +3,21 @@ const Home = () =>{
 
     return (
         <div className={styles.HomePage}>
-            <h1 className="">this is the home page</h1>
+
             <div className={styles.sidebar}>
                 <ul>
-                    <li>Heading 1</li>
-                    <li>Heading 2</li>
-                    <li>Heading 3</li>
-                    <li>Heading 4</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia nobis earum consequatur impedit assumenda, quis dolorem sint sequi. Possimus error nam incidunt itaque rerum! Quis amet aspernatur ducimus excepturi officiis!</li>
+                    <li>heading 2</li>
+                    <li>heading 3</li>
+                    <li>heading 4</li>
+                    <li>heading 5</li>
+                    <li>heading 6</li>
                 </ul>
             </div>
+            <div className={styles.content}>
+                <h1>Content part</h1>
+            </div>
+
 
         </div>
 
